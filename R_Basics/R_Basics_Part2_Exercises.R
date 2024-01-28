a471fdd6532d3921c8a7655eb7ac3fb90ff24334
A Cup Of Coding <152088515+A-Cup-Of-Coding@users.noreply.github.com>
2024-01-28 23:58:22 +0100

    Add files via upload

---
+++ R_Basics/R_Basics_Part2_Exercises.R
@@ -0,0 +1,92 @@
+######## SECTION 1: VECTORS ##########
+
+# Create a vector containing strings
+
+
+# Create a vector containing a range of numeric values
+
+
+# Select one item from one of the vectors
+
+
+# Select multiple items from one of the vectors
+
+
+# Change an item from one of the vectors
+
+
+# Repeat one of the vectors
+
+
+# Create a vector that counts from 0 to 200 with steps of 25
+
+
+######## SECTION 2: LISTS ##########
+
+# Create a list containing strings
+
+
+# Select one of the items in the list
+
+
+# Replace one item in the list
+
+
+# Add an item to the list
+
+# Remove an item from the list
+
+
+######## SECTION 3: MATRICES ##########
+
+# Create a matrix that contains the number 1 to 20, has 5 rows and 4 columns
+
+
+# Select the number 14 from this matrix
+
+
+# Select the second column of the matrix
+
+
+# Add a 5th column with the numbers 21 to 25
+
+
+# Add a 6th row with only zero's
+
+
+# Remove the row with zero's from the Matrix
+
+
+
+######## SECTION 4: ARRAYS ##########
+
+# Create a array one that contains 2 matrices of 3 rows and 2 columns
+
+
+# Select the second column of the first matrix
+
+
+
+######## SECTION 5: DATA FRAMES ##########
+
+# Create a dataframes with three columns, each with a different datatype
+
+
+# Select the 2nd column of your data frame with three different methods.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
